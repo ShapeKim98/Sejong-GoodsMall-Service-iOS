aa91099fbe7df2c5e6b79b91398194d5237e367e
ShapeKim98 <talkers.potters-0p@icloud.com>
2023-01-19 18:58:01 +0900

    initial Project

---
+++ SejongUniversityGoodsMall/SejongUniversityGoodsMall/SejongUniversityGoodsMallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SejongUniversityGoodsMallApp.swift
+//  SejongUniversityGoodsMall
+//
+//  Created by 김도형 on 2023/01/19.
+//
+
+import SwiftUI
+
+@main
+struct SejongUniversityGoodsMallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
